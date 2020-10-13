b0ce4e5f901f29afb63c174c16966a1bf44089c1
Perry Chouteau <perry.chouteau@epitech.eu>
2020-10-13 17:17:05 +0200

    Library

---
+++ Library tuesday 13 oct 2020/lib/my/my_strcnpy.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** my_strncpy
+** File description:
+** function that copies n charactere of a string into another
+*/
+
+char *my_strncpy(char *dest, char  const *src,int n)
+{
+    int i;
+    for(i = 0; i <= n; i += 1) {
+        dest[i] = src[i];
+    }
+    dest[i-1] = '\0';
+    return dest;
+}
